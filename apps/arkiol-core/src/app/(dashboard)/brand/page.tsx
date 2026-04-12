062ee79f0723913eece2c02871b4e3944b9a50e6
arkiol37 <arkiol37@gmail.com>
2026-04-12 05:22:21 +0500

    Delete apps/arkiol-core/src directory

--- apps/arkiol-core/src/app/(dashboard)/brand/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/(dashboard)/brand/page.tsx
-export const dynamic = "force-dynamic";
-
-import { BrandKitView } from "../../../components/dashboard/BrandKitView";
-
-export default function Page() {
-  return <BrandKitView />;
-}
--- apps/arkiol-core/src/app/(dashboard)/gallery/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/(dashboard)/gallery/page.tsx
-import { GalleryView } from "../../../components/dashboard/GalleryView";
-
-export default function Page() {
-  return <GalleryView />;
-}
